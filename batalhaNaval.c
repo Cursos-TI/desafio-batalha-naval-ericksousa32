4645373512198181d4d9a569c4576b9fa0d6df41
Erick Sousa <erickwilliams879@gmail.com>
2025-07-25 00:41:30 +0000

    Nível Aventureiro Implementado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 
+#define TAM 10
+
 int main(){
-    char coluna[10] = {'A','B','C','D','E','F','G','H','I','J'}; // Este vetor demarcará as colunas visualmente.
-    int tabuleiro[10][10]; // Declaração da matriz que servirá como tabuleiro.
+    char coluna[TAM] = {'A','B','C','D','E','F','G','H','I','J'}; // Este vetor demarcará as colunas visualmente.
+    int tabuleiro[TAM][TAM]; // Declaração da matriz que servirá como tabuleiro.
 
     int navio1[3] = {3,3,3}; // Inicialização do vetor correspodente ao navio que ficará na horizontal.
     int navio2[3] = {3,3,3}; // Inicialização do vetor correspodente ao navio que ficará na vertical.
+    int navio3[3] = {3,3,3}; // Inicialização do vetor correspodente ao navio que ficará na diagonal principal.
+    int navio4[3] = {3,3,3}; // Inicialização do vetor correspodente ao navio que ficará na diagonal secundária.
 
-    int pos1[2] = {1,3}; // Define a posição inicial (x,y) do navio horizontal.
-    int pos2[2] = {5,2}; // Define a posição inicial (x.y) do navio vertical.
+    int pos1[2] = {4,4}; // Define a posição inicial (x,y) do navio horizontal.
+    int pos2[2] = {2,2}; // Define a posição inicial (x.y) do navio vertical.
+    int pos3[2] = {7,7}; // Define a posição inicial (x.y) do navio na diagonal principal.
+    int pos4[2] = {2,7}; // Define a posição inicial (x.y) do navio na diagonal secundária.
 
     int flag = 0; // Flag que verifica se o navio foi inserido numa posição inválida.
 
     printf("***TABULEIRO DE BATALHA NAVAL***\n\n");
 
-    for(int i = 0; i < 10; i++){ // Preenche todo o tabuleiro 10x10 com valor 0.
-        for(int j = 0; j < 10; j++){
+    for(int i = 0; i < TAM; i++){ // Preenche todo o tabuleiro TAMxTAM com valor 0.
+        for(int j = 0; j < TAM; j++){
             tabuleiro[i][j] = 0;
         }
     }
 
     printf("   ");
-    for(int i = 0; i < 10; i++){ //Imprime a demarcação das colunas de A a J.
+    for(int i = 0; i < TAM; i++){ //Imprime a demarcação das colunas de A a J.
         printf("%c ", coluna[i]);
     }
     printf("\n");
 
-    for(int i = 0; i < 10; i++){ // Loop externo responsável pela inserção do navio horizontal.
-        for(int j = 0; j < 10; j++){ // Loop interno responsável pela inserção do navio horizontal.
+    for(int i = 0; i < TAM; i++){ // Loop externo responsável pela inserção do navio horizontal.
+        for(int j = 0; j < TAM; j++){ // Loop interno responsável pela inserção do navio horizontal.
             if(i == pos1[0] && j == pos1[1]){ // Busca a posição inicial do navio.
                 if(pos1[1] > 7){ // Verifica se o navio ficará dentro do tabuleiro (levando em conta que o tamanho fixo dos navios é 3), se não, a flag será acrescentada.
                     flag++;
@@ -44,8 +50,8 @@ int main(){
         }
     }
 
-    for(int i = 0; i < 10; i++){ // Loop externo responsável pela inserção do navio vertical.
-        for(int j = 0; j < 10; j++){ // Loop interno responsável pela inserção do navio vertical.
+    for(int i = 0; i < TAM; i++){ // Loop externo responsável pela inserção do navio vertical.
+        for(int j = 0; j < TAM; j++){ // Loop interno responsável pela inserção do navio vertical.
             if(i == pos2[0] && j == pos2[1]){ // Busca a posição inicial do navio.
                 if(pos2[0] > 7){ // Verifica se o navio ficará dentro do tabuleiro (levando em conta que o tamanho fixo dos navios é 3), se não, a flag será acrescentada.
                     flag++;
@@ -55,20 +61,56 @@ int main(){
                     flag++;
                     break;
                 }
-                tabuleiro[i][j] = navio1[0]; // Inserção do navio.
-                tabuleiro[i+1][j] = navio1[1];
-                tabuleiro[i+2][j] = navio1[2];
+                tabuleiro[i][j] = navio2[0]; // Inserção do navio.
+                tabuleiro[i+1][j] = navio2[1];
+                tabuleiro[i+2][j] = navio2[2];
             }
         }
     }
 
-    for(int i = 0; i < 10; i++){ // Imprime todo o tabuleiro.
+    for(int i = 0; i < TAM; i++){ // Loop externo responsável pela inserção do navio na diagonal principal.
+        for(int j = 0; j < TAM; j++){ // Loop interno responsável pela inserção do navio na diagonal principal.
+            if(i == pos3[0] && j == pos3[1]){ // Busca a posição inicial do navio.
+                if(pos3[0] > 7 || pos3[1] > 7){ // Verifica se o navio ficará dentro do tabuleiro (levando em conta que o tamanho fixo dos navios é 3), se não, a flag será acrescentada.
+                    flag++;
+                    break;
+                }
+                if(tabuleiro[i][j] != 0 || tabuleiro[i+1][j+1] != 0 || tabuleiro[i+2][j+2] != 0){ // Verifica se o campo está livre para inserção, se não estiver, a flag será acrescentada.
+                    flag++;
+                    break;
+                }
+                tabuleiro[i][j] = navio3[0]; // Inserção do navio.
+                tabuleiro[i+1][j+1] = navio3[1];
+                tabuleiro[i+2][j+2] = navio3[2];
+            }
+        }
+    }
+
+    for(int i = 0; i < TAM; i++){ // Loop externo responsável pela inserção do navio na diagonal secundária.
+        for(int j = 0; j < TAM; j++){ // Loop interno responsável pela inserção do navio na diagonal secundária.
+            if(i == pos4[0] && j == pos4[1]){ // Busca a posição inicial do navio.
+                if(pos4[0] < 2 || pos4[1] > 7){ // Verifica se o navio ficará dentro do tabuleiro (levando em conta que o tamanho fixo dos navios é 3), se não, a flag será acrescentada.
+                    flag++;
+                    break;
+                }
+                if(tabuleiro[i][j] != 0 || tabuleiro[i-1][j+1] != 0 || tabuleiro[i-2][j+2] != 0){ // Verifica se o campo está livre para inserção, se não estiver, a flag será acrescentada.
+                    flag++;
+                    break;
+                }
+                tabuleiro[i][j] = navio4[0]; // Inserção do navio.
+                tabuleiro[i-1][j+1] = navio4[1];
+                tabuleiro[i-2][j+2] = navio4[2];
+            }
+        }
+    }
+
+    for(int i = 0; i < TAM; i++){ // Imprime todo o tabuleiro.
         if(i != 9){
-            printf(" %d ", i+1);
+            printf("%2d ", i+1);
         }else{
             printf("%d ", i+1);
         }
-        for(int j = 0; j < 10; j++){
+        for(int j = 0; j < TAM; j++){
             printf("%d ",tabuleiro[i][j]);
         }
         printf("\n");
